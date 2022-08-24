66c725f2c009cac2f05384daba62052c9d78bbb6
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2022-08-24 15:04:01 +0500

    commit

--- src/Component/child.js
+++ src/Component/child.js
@@ -3,7 +3,8 @@ import React from 'react'
 function child(props) {
   return (
     <div>
-        <button onClick={()=>props.passmethodasprop('and then From child to parent')}> Greet Parent from child</button>
+        <button onClick={()=>props.passmethodasprop('and then pass parameter From child to parent')}> 
+        Greet Parent from child</button>
     </div>
   )
 }
